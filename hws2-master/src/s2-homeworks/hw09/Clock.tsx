cbdeda91047f4ad5fe4bfddb2a27c3f85abf4c78
Inna Groznaya <inna2011@tut.by>
2023-02-19 18:23:44 +0300

    resolved HW9

--- hws2-master/src/s2-homeworks/hw09/Clock.tsx
+++ hws2-master/src/s2-homeworks/hw09/Clock.tsx
@@ -29,9 +29,9 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = date.toLocaleTimeString()
+    const stringTime = date.toLocaleTimeString('ru-Ru')
     // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = date.toLocaleDateString('ru-Ru') // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)// пишут студенты
